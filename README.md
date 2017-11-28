f73cebd0a8b56567b725d496c4c36ff1813b3ba7
Andrew <andrew@andrewt.net>
2017-11-28 18:49:57 +0000

    Reduce flicker and increase browser support

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Just a quick scrobbler thing on the slow-mo video me and Ash took at MathsJam of the chain ring trick. (I think the hand is Will?) Scroll horizontally to send the ring up and down the chain.
 
-Currently Firefox only because it's bedtime.
+Currently tested in Firefox and Chrome. Note that it's designed for natural-scrolling trackpads and touchscreens so it will feel a bit backwards on desktop (unless you come from a right-to-left reading culture in which case I don't know how you would normally expect this sort of thing to work).
 
 I should probably warn you, at this framerate the 50Hz mains light flicker might act as a strobe. I've not had chance to clean that up yet.
 
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', e => {
-	const imgContainer = document.getElementById('image');
+	const imgContainer = document.getElementById('image'),
+		hiddenImgContainer = document.getElementById('constant-image');
 	const images = [],
 		promises = [];
+	let allLoaded = false,
+		maxScroll;
 	for (let f = 302; f < 419; ++f) {
 		const img = document.createElement('img');
 		img.setAttribute('src', `img/img0${f}.jpg`);
@@ -12,9 +15,12 @@ document.addEventListener('DOMContentLoaded', e => {
 			img.addEventListener('error', e => reject(e));
 		}));
 	}
+	// Copy the images into another layer which is
+	// always "visible" so the browser doesn't unload them:
+	hiddenImgContainer.innerHTML = imgContainer.innerHTML;
 
 	function update() {
-		const pos = window.scrollX / window.window.scrollMaxX;
+		const pos = window.scrollX / maxScroll;
 		let frame = Math.floor((1 - pos) * images.length);
 		if (frame < 0) frame = 0;
 		if (frame >= images.length) frame = images.length - 1;
@@ -24,13 +30,25 @@ document.addEventListener('DOMContentLoaded', e => {
 		});
 	}
 
+	function resize() {
+		maxScroll = Math.max(
+			window.scrollMaxX || 0,
+			(document.body.scrollWidth || 0) - window.innerWidth,
+			(document.getElementById('scroller').scrollWidth || 0) - window.innerWidth);
+		if (allLoaded)
+			update();
+	}
+	window.addEventListener('resize', resize);
+
 	Promise.all(promises)
 		.then(() => {
+			allLoaded = true;
+			resize();
 			document.addEventListener('scroll', update);
 			window.addEventListener('scroll', update);
 			document.body.addEventListener('scroll', update);
 			window.addEventListener('resize', update);
-			window.scroll(window.scrollMaxX, 0);
+			window.scroll(maxScroll, 0);
 			document.getElementById('loading').style.display = 'none';
 		})
 		.catch(e => {
